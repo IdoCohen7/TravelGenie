c67d58c74a53486f6f3d834bd737a9ddf41b0a02
Ido Cohen <idocohen727@gmail.com>
2025-08-30 19:35:58 +0300

    fix

--- Server/Server/BL/DTOs.cs
+++ Server/Server/BL/DTOs.cs
@@ -35,12 +35,6 @@
         public decimal? EstCost { get; set; }
     }
 
-    public class LoginRequest
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-
     public record TripPlan(
     string? currency,
     decimal? totalEstCost,
